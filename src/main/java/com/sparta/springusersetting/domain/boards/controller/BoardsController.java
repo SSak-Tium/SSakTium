ddce766238faf695e34c2e8b33297025f91a661e
checkuu-0216 <corb37@gmail.com>
2024-10-22 23:57:35 +0900

    [Add] (Board) 보드 조회기능들 생성(댓글,친구 빠져있음)

--- src/main/java/com/sparta/springusersetting/domain/boards/controller/BoardsController.java
+++ src/main/java/com/sparta/springusersetting/domain/boards/controller/BoardsController.java
@@ -7,9 +7,12 @@ import com.sparta.springusersetting.domain.boards.dto.responseDto.BoardsSaveResp
 import com.sparta.springusersetting.domain.boards.service.BoardsService;
 import com.sparta.springusersetting.domain.common.dto.AuthUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +21,23 @@ public class BoardsController {
 
     private final BoardsService boardsService;
 
+    /**
+     * 게시글 작성
+     * @param authUser
+     * @RequestBody requestDto
+     */
     @PostMapping()
     public ResponseEntity<ApiResponse<BoardsSaveResponseDto>> saveBoard(@AuthenticationPrincipal AuthUser authUser,
                                                                         @RequestBody BoardsSaveRequestDto requestDto){
         return  ResponseEntity.ok(ApiResponse.success(boardsService.saveBoards(authUser,requestDto)));
     }
 
+    /**
+     * 게시글 수정
+     * @param authUser
+     * @param id
+     * @param requestDto
+     */
     @PutMapping("/{id}")
     public ResponseEntity<ApiResponse<BoardsSaveResponseDto>> updateBoard(@AuthenticationPrincipal AuthUser authUser,
                                                                            @PathVariable Long id,
@@ -31,6 +45,11 @@ public class BoardsController {
         return ResponseEntity.ok(ApiResponse.success(boardsService.updateBoards(authUser,id,requestDto)));
     }
 
+    /**
+     * 게시글 삭제변환
+     * @param authUser
+     * @param id
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Void>> deleteBoard(@AuthenticationPrincipal AuthUser authUser,
                                                        @PathVariable Long id){
@@ -38,8 +57,38 @@ public class BoardsController {
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<BoardsDetailResponseDto>> getBoard(@PathVariable Long id){
-        boardsService.getBoard();
+    //게시글 단건 조회 (댓글 빠져있음)
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ApiResponse<BoardsDetailResponseDto>> getBoard(@PathVariable Long id){
+//        boardsService.getBoard();
+//        return ResponseEntity.ok(ApiResponse.success(boardsService.getBoard()));
+//    }
+
+    /**
+     * 내 게시글 조회
+     * @param authUser
+     * @param page
+     * @param size
+     * 댓글 페이지네이션 빠져있음
+     */
+    @GetMapping("/my-boards")
+    public ResponseEntity<ApiResponse<Page<BoardsDetailResponseDto>>> getMyBoards(@AuthenticationPrincipal AuthUser authUser,
+                                                                                @RequestParam(defaultValue = "1") int page,
+                                                                                @RequestParam(defaultValue = "5") int size){
+        return ResponseEntity.ok(ApiResponse.success(boardsService.getMyBoards(authUser,page,size)));
     }
+
+    /**
+     * 친구항목 필요
+     * @param authUser
+     * @param page
+     * @param size
+     */
+//    @GetMapping("/newsfeed")
+//    public ResponseEntity<ApiResponse<Page<BoardsDetailResponseDto>>> getNewsfeed(@AuthenticationPrincipal AuthUser authUser,
+//                                                                                  @RequestParam(defaultValue = "1") int page,
+//                                                                                  @RequestParam(defaultValue = "5") int size){
+//        return ResponseEntity.ok(ApiResponse.success(boardsService.getNewsfeed(authUser,page,size)));
+//    }
+
 }
--- src/main/java/com/sparta/springusersetting/domain/boards/service/BoardsService.java
+++ src/main/java/com/sparta/springusersetting/domain/boards/service/BoardsService.java
@@ -8,17 +8,13 @@ import com.sparta.springusersetting.domain.boards.exception.NotFoundBoardExcepti
 import com.sparta.springusersetting.domain.boards.repository.BoardsRepository;
 import com.sparta.springusersetting.domain.common.dto.AuthUser;
 import com.sparta.springusersetting.domain.user.entity.User;
-import com.sparta.springusersetting.domain.user.repository.UserRepository;
 import com.sparta.springusersetting.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Comments;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PutMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -72,7 +68,7 @@ public class BoardsService {
         boardsRepository.save(deleteBoards);
     }
 
-//    //게시글 단건 조회
+//    //게시글 단건 조회 (댓글필요)
 //    public BoardsDetailResponseDto getBoard(Long id) {
 //        //게시글 찾기
 //       Boards board = findBoard(id);
@@ -87,10 +83,44 @@ public class BoardsService {
 //        return  new BoardsDetailResponseDto(board,dtoList);
 //    }
 
+    public Page<BoardsDetailResponseDto> getMyBoards(AuthUser authUser, int page, int size) {
+        //사용자 찾기
+        User user = userService.findUser(authUser.getUserId());
+        //페이지 요청 객체 생성 (페이지 숫자가 실제로는 0부터 시작하므로 원하는 숫자 -1을 입력해야 해당 페이지가 나온다)
+        Pageable pageable = PageRequest.of(page -1, size);
+        //해당 유저가 쓴 게시글 페이지네이션해서 가져오기
+        Page<Boards> boards = boardsRepository.findAllByUserId(user.getId(),pageable);
+        //찾은게시글들 responseDto에 맞춰서 반환하기
+        return boards.map(BoardsDetailResponseDto::new);
+    }
+
+    //뉴스피드 (친구필요)
+//    public Page<BoardsDetailResponseDto> getNewsfeed(AuthUser authUser, int page, int size) {
+//        //사용자 찾기
+//        User user = userService.findUser(authUser.getUserId());
+//        Pageable pageable = PageRequest.of(page - 1 ,size);
+//
+//        //친구목록 가져오기
+//        List<User> friends = userService.getFriends(user.getId());
+//
+//        // 게시글 리스트 가져오기
+//        Page<Boards> boardsPage = boardsRepository.findAllForNewsFeed(
+//                user,
+//                friends,
+//                PublicStatus.FRIENDS, // 친구의 게시글 상태
+//                PublicStatus.ALL,      // 전체 공개 게시글 상태
+//                pageable               // Pageable 객체 전달
+//        );
+//
+//        // DTO로 변환하여 반환
+//        return boardsPage.map(BoardsDetailResponseDto::new);
+//    }
 
     //Board 찾는 메서드
     public Boards findBoard(long id){
        return boardsRepository.findById(id).orElseThrow(NotFoundBoardException::new);
     }
+
+
 
 }
