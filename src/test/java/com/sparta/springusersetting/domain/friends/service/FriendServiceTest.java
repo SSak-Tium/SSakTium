c992ca3320bf0de203bb937c9840798871d48cea
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-10-23 19:56:54 +0900

    [FEAT] 친구서비스테스트 생성

---
+++ src/test/java/com/sparta/springusersetting/domain/friends/service/FriendServiceTest.java
@@ -0,0 +1,35 @@
+package com.sparta.springusersetting.domain.friends.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FriendServiceTest {
+
+    @Test
+    void requestFriend() {
+    }
+
+    @Test
+    void cancelFriend() {
+    }
+
+    @Test
+    void acceptFriend() {
+    }
+
+    @Test
+    void rejectFriend() {
+    }
+
+    @Test
+    void getFriends() {
+    }
+
+    @Test
+    void deleteFriend() {
+    }
+}
