3f2dda2bfb184f7c0bf3f004b8abfea1e0eb57e5
lklll1212 <lklll1212@gmail.com>
2024-10-23 19:43:34 +0900

    [feat] 좋아요 등록(게시글) 게시글에 좋아요 등록

--- src/main/java/com/sparta/springusersetting/domain/boards/entity/Board.java
+++ src/main/java/com/sparta/springusersetting/domain/boards/entity/Board.java
@@ -3,9 +3,10 @@ package com.sparta.springusersetting.domain.boards.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 @Entity
 @NoArgsConstructor
@@ -15,4 +16,10 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private int boardLikesCount =0;
+
+    public void plusLikesCount(){
+        boardLikesCount++;
+    }
 }
--- src/main/java/com/sparta/springusersetting/domain/likes/boardLikes/controller/BoardLikesController.java
+++ src/main/java/com/sparta/springusersetting/domain/likes/boardLikes/controller/BoardLikesController.java
@@ -1,13 +1,16 @@
 package com.sparta.springusersetting.domain.likes.boardLikes.controller;
 
 import com.sparta.springusersetting.config.ApiResponse;
+import com.sparta.springusersetting.domain.common.dto.AuthUser;
 import com.sparta.springusersetting.domain.likes.boardLikes.dto.BoardLikesResponseDto;
 import com.sparta.springusersetting.domain.likes.boardLikes.service.BoardLikesService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Getter
@@ -19,8 +22,16 @@ public class BoardLikesController {
 
     // 좋아요 조회
     @GetMapping
-    public ResponseEntity<ApiResponse<BoardLikesResponseDto>> getBoardLikes(@PathVariable Long boardId){
-        return ResponseEntity.ok(ApiResponse.success(boardLikesService.getBoardLikes(boardId)));
+    public ResponseEntity<ApiResponse<BoardLikesResponseDto>> getBoardLikes(@PathVariable Long boardId,
+                                                                            @AuthenticationPrincipal AuthUser authUser){
+        return ResponseEntity.ok(ApiResponse.success(boardLikesService.getBoardLikes(boardId,authUser)));
+    }
+
+    // 좋아요 등록
+    @PostMapping
+    public ResponseEntity<ApiResponse<BoardLikesResponseDto>> postBoardLikes(@PathVariable Long boardId,
+                                                                             @AuthenticationPrincipal AuthUser authUser){
+        return ResponseEntity.ok(ApiResponse.success(boardLikesService.postBoardLikes(boardId,authUser)));
     }
 
 }
