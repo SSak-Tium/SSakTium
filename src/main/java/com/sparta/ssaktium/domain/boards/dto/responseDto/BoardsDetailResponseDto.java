912dcc4f6a77bff876f563668a7a80aa884acbc3
checkuu-0216 <corb37@gmail.com>
2024-10-24 11:52:01 +0900

    [Fix] (boards) 시간 보이기

--- src/main/java/com/sparta/ssaktium/domain/boards/dto/responseDto/BoardsDetailResponseDto.java
+++ src/main/java/com/sparta/ssaktium/domain/boards/dto/responseDto/BoardsDetailResponseDto.java
@@ -3,6 +3,8 @@ package com.sparta.ssaktium.domain.boards.dto.responseDto;
 import com.sparta.ssaktium.domain.boards.entity.Boards;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class BoardsDetailResponseDto {
 
@@ -10,11 +12,15 @@ public class BoardsDetailResponseDto {
     private final String title;
     private final String contents;
     private final String images;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
     public BoardsDetailResponseDto(Boards boards) {
         this.id = boards.getId();
         this.title = boards.getTitle();
         this.contents = boards.getContent();
         this.images = boards.getImage();
+        this.createdAt = boards.getCreatedAt();
+        this.modifiedAt = boards.getModifiedAt();
     }
 }
--- src/main/java/com/sparta/ssaktium/domain/boards/dto/responseDto/BoardsSaveResponseDto.java
+++ src/main/java/com/sparta/ssaktium/domain/boards/dto/responseDto/BoardsSaveResponseDto.java
@@ -3,6 +3,8 @@ package com.sparta.ssaktium.domain.boards.dto.responseDto;
 import com.sparta.ssaktium.domain.boards.entity.Boards;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class BoardsSaveResponseDto {
 
@@ -10,11 +12,13 @@ public class BoardsSaveResponseDto {
     public final String title;
     public final String contents;
     public final String images;
+    public final LocalDateTime createdAt;
 
     public BoardsSaveResponseDto(Boards boards) {
         this.id = boards.getId();
         this.title = boards.getTitle();
         this.contents = boards.getContent();
         this.images = boards.getImage();
+        this.createdAt = boards.getCreatedAt();
     }
 }
