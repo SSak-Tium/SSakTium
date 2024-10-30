df6b8a1b8455a9692528a9af7adb66bdfa9f424d
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-10-30 17:56:20 +0900

    [FIX] (friend) 서비스테스트코드

--- src/test/java/com/sparta/ssaktium/domain/friends/service/FriendServiceTest.java
+++ src/test/java/com/sparta/ssaktium/domain/friends/service/FriendServiceTest.java
@@ -1,272 +1,213 @@
-//package com.sparta.ssaktium.domain.friends.service;
-//
-//import com.sparta.ssaktium.domain.common.dto.AuthUser;
-//import com.sparta.ssaktium.domain.friends.dto.responseDto.FriendPageResponseDto;
-//import com.sparta.ssaktium.domain.friends.dto.responseDto.FriendResponseDto;
-//import com.sparta.ssaktium.domain.friends.entity.Friend;
-//import com.sparta.ssaktium.domain.friends.entity.FriendStatus;
-//import com.sparta.ssaktium.domain.friends.exception.FriendRequestAlreadySentException;
-//import com.sparta.ssaktium.domain.friends.exception.NotFoundFriendException;
-//import com.sparta.ssaktium.domain.friends.exception.NotFoundRequestFriendException;
-//import com.sparta.ssaktium.domain.friends.exception.SelfRequestException;
-//import com.sparta.ssaktium.domain.friends.repository.FriendRepository;
-//import com.sparta.ssaktium.domain.users.entity.User;
-//import com.sparta.ssaktium.domain.users.enums.UserRole;
-//import com.sparta.ssaktium.domain.users.service.UserService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.data.domain.PageRequest;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class FriendServiceTest {
-//
-//    @Mock
-//    private FriendRepository friendRepository;
-//
-//    @Mock
-//    private UserService userService;
-//
-//    @InjectMocks
-//    private FriendService friendService;
-//
-//
-//    private AuthUser authUser;
-//    private AuthUser friendAuthUser;
-//
-//    private User user;
-//    private User friendUser;
-//
-//    @BeforeEach
-//    void setUp() {
-//        authUser = new AuthUser(1L, "11@11.com", UserRole.ROLE_USER);
-//        friendAuthUser = new AuthUser(2L, "22@22.com", UserRole.ROLE_USER);
-//
-//        // User 객체 생성
-//        user = User.fromAuthUser(authUser);
-//        friendUser = User.fromAuthUser(friendAuthUser);
-//    }
-//
-//    @Test
-//    void 친구_요청_성공() {
-//        // given
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(userService.findUser(friendAuthUser.getUserId())).thenReturn(friendUser);
-//        when(friendRepository.findByUserIdAndFriendUserId(user.getId(), friendUser.getId())).thenReturn(Optional.empty());
-//
-//        // when
-//        FriendResponseDto responseDto = friendService.requestFriend(authUser.getUserId(), friendAuthUser.getUserId());
-//
-//        // then
-//        assertNotNull(responseDto);
-//        assertEquals(responseDto.getMyUserId(), authUser.getUserId());
-//        assertEquals(responseDto.getFriendUserId(), friendAuthUser.getUserId());
-//        assertEquals(responseDto.getStatus(), FriendStatus.PENDING);
-//
-//        // verify that the friend was saved
-//        verify(friendRepository).save(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 이미_보낸_친구_요청_예외() {
-//        // given
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(userService.findUser(friendAuthUser.getUserId())).thenReturn(friendUser);
-//        when(friendRepository.findByUserIdAndFriendUserId(user.getId(), friendUser.getId()))
-//                .thenReturn(Optional.of(new Friend(user, friendUser)));
-//
-//        // when & then
-//        assertThrows(FriendRequestAlreadySentException.class, () -> {
-//            friendService.requestFriend(authUser.getUserId(), friendAuthUser.getUserId());
-//        });
-//
-//        verify(friendRepository, never()).save(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 자기자신에게_친구_요청_예외() {
-//        // given
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//
-//        // when & then
-//        assertThrows(SelfRequestException.class, () -> {
-//            friendService.requestFriend(authUser.getUserId(), authUser.getUserId());
-//        });
-//
-//        verify(friendRepository, never()).save(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 친구_요청_취소_성공() {
-//        // given
-//        Long friendUserId = 2L;
-//        User friendUser = User.fromAuthUser(friendAuthUser);
-//        Friend friend = new Friend(user, friendUser);
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(user.getId(), friendUserId)).thenReturn(Optional.of(friend));
-//
-//        // when
-//        friendService.cancelFriend(authUser.getUserId(), friendUserId);
-//
-//        // then
-//        verify(friendRepository).delete(friend);
-//    }
-//
-//    @Test
-//    void 친구_요청_취소_실패_친구_요청_없음() {
-//        // given
-//        Long friendUserId = 2L;
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(user.getId(), friendUserId)).thenReturn(Optional.empty());
-//
-//        // when & then
-//        assertThrows(NotFoundRequestFriendException.class, () -> friendService.cancelFriend(authUser.getUserId(), friendUserId));
-//        verify(friendRepository, never()).delete(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 친구_요청_수락_성공() {
-//        // given
-//        Long friendUserId = 2L;
-//        User friendUser = User.fromAuthUser(friendAuthUser);
-//        Friend friend = new Friend(friendUser, user);
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(friendUserId, user.getId())).thenReturn(Optional.of(friend));
-//
-//        // when
-//        FriendResponseDto responseDto = friendService.acceptFriend(authUser.getUserId(), friendUserId);
-//
-//        // then
-//        assertNotNull(responseDto);
-//        assertEquals(FriendStatus.ACCEPTED, friend.getFriendStatus());
-//        verify(friendRepository).save(friend);
-//    }
-//
-//    @Test
-//    void 친구_요청_수락_실패_요청_없음() {
-//        // given
-//        Long friendUserId = 2L;
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(friendUserId, user.getId())).thenReturn(Optional.empty());
-//
-//        // when & then
-//        assertThrows(NotFoundRequestFriendException.class, () -> friendService.acceptFriend(authUser.getUserId(), friendUserId));
-//        verify(friendRepository, never()).save(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 친구_요청_거절_성공() {
-//        // given
-//        Long friendUserId = 2L;
-//        User friendUser = User.fromAuthUser(friendAuthUser);
-//        Friend friend = new Friend(friendUser, user);
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(friendUserId, user.getId())).thenReturn(Optional.of(friend));
-//
-//        // when
-//        FriendResponseDto responseDto = friendService.rejectFriend(authUser.getUserId(), friendUserId);
-//
-//        // then
-//        assertNotNull(responseDto);
-//        assertEquals(FriendStatus.REJECTED, friend.getFriendStatus());
-//        verify(friendRepository).save(friend);
-//    }
-//
-//    @Test
-//    void 친구_요청_거절_실패_요청_없음() {
-//        // given
-//        Long friendUserId = 2L;
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendUserId(friendUserId, user.getId())).thenReturn(Optional.empty());
-//
-//        // when & then
-//        assertThrows(NotFoundRequestFriendException.class, () -> friendService.rejectFriend(authUser.getUserId(), friendUserId));
-//        verify(friendRepository, never()).save(any(Friend.class));
-//    }
-//
-//    @Test
-//    void 친구_목록_조회_성공() {
-//        // given
-//        PageRequest pageRequest = PageRequest.of(0, 10);
-//        Friend friend = new Friend(user, friendUser);
-//        Page<Friend> friendsPage = new PageImpl<>(List.of(friend));
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdOrFriendIdAndStatus(user.getId(), user.getId(), FriendStatus.ACCEPTED, pageRequest))
-//                .thenReturn(friendsPage);
-//
-//        // when
-//        Page<FriendPageResponseDto> result = friendService.getFriends(authUser.getUserId(), 1, 10);
-//
-//        // then
-//        assertNotNull(result);
-//        assertEquals(1, result.getTotalElements());
-//    }
-//
-//    @Test
-//    void 친구_목록_조회_친구_없음() {
-//        // given
-//        PageRequest pageRequest = PageRequest.of(0, 10);
-//        Page<Friend> friendsPage = Page.empty();
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdOrFriendIdAndStatus(user.getId(), user.getId(), FriendStatus.ACCEPTED, pageRequest))
-//                .thenReturn(friendsPage);
-//
-//        // when
-//        Page<FriendPageResponseDto> result = friendService.getFriends(authUser.getUserId(), 1, 10);
-//
-//        // then
-//        assertNotNull(result);
-//        assertEquals(0, result.getTotalElements());
-//    }
-//
-//    @Test
-//    void 친구_삭제_성공() {
-//        // given
-//        Long friendUserId = 2L;
-//        User friendUser = User.fromAuthUser(friendAuthUser);
-//        Friend friend = new Friend(user, friendUser);
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendId(user.getId(), friendUserId)).thenReturn(Optional.of(friend));
-//
-//        // when
-//        friendService.deleteFriend(authUser.getUserId(), friendUserId);
-//
-//        // then
-//        verify(friendRepository).delete(friend);
-//    }
-//
-//    @Test
-//    void 친구_삭제_실패_친구_없음() {
-//        // given
-//        Long friendUserId = 2L;
-//
-//        when(userService.findUser(authUser.getUserId())).thenReturn(user);
-//        when(friendRepository.findByUserIdAndFriendId(user.getId(), friendUserId)).thenReturn(Optional.empty());
-//        when(friendRepository.findByUserIdAndFriendId(friendUserId, user.getId())).thenReturn(Optional.empty());
-//
-//        // when & then
-//        assertThrows(NotFoundFriendException.class, () -> friendService.deleteFriend(authUser.getUserId(), friendUserId));
-//        verify(friendRepository, never()).delete(any(Friend.class));
-//    }
-//
-//}
+package com.sparta.ssaktium.domain.friends.service;
+
+import com.sparta.ssaktium.domain.common.dto.AuthUser;
+import com.sparta.ssaktium.domain.friends.dto.responseDto.FriendPageResponseDto;
+import com.sparta.ssaktium.domain.friends.dto.responseDto.FriendResponseDto;
+import com.sparta.ssaktium.domain.friends.entity.Friend;
+import com.sparta.ssaktium.domain.friends.entity.FriendStatus;
+import com.sparta.ssaktium.domain.friends.exception.*;
+import com.sparta.ssaktium.domain.friends.repository.FriendRepository;
+import com.sparta.ssaktium.domain.users.entity.User;
+import com.sparta.ssaktium.domain.users.enums.UserRole;
+import com.sparta.ssaktium.domain.users.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class FriendServiceTest {
+
+    @InjectMocks
+    private FriendService friendService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private FriendRepository friendRepository;
+
+    private AuthUser authUser;
+    private AuthUser friendAuthUser;
+    private Friend friend;
+    private User user;
+    private User friendUser;
+
+    @BeforeEach
+    void setUp() {
+        authUser = new AuthUser(1L, "user1@test.com", UserRole.ROLE_USER);
+        friendAuthUser = new AuthUser(2L, "user2@test.com", UserRole.ROLE_USER);
+        user = User.fromAuthUser(authUser);
+        friendUser = User.fromAuthUser(friendAuthUser);
+        friend = new Friend(user, friendUser);
+    }
+
+    @Nested
+    class 친구요청_테스트 {
+        @Test
+        void 친구요청_성공() {
+            // Given
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(userService.findUser(friendAuthUser.getUserId())).willReturn(friendUser);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.empty());
+            given(friendRepository.save(any(Friend.class)))
+                    .willAnswer(invocation -> invocation.getArgument(0));
+
+            // When
+            FriendResponseDto response = friendService.requestFriend(authUser.getUserId(), friendAuthUser.getUserId());
+
+            // Then
+            assertThat(response).isNotNull();
+            verify(friendRepository).save(any(Friend.class));
+        }
+
+        @Test
+        void 자기자신에게_친구요청시_예외발생() {
+            // When & Then
+            assertThrows(SelfRequestException.class,
+                    () -> friendService.requestFriend(authUser.getUserId(), authUser.getUserId()));
+        }
+
+        @Test
+        void 이미존재하는_친구요청시_예외발생() {
+            // Given
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(userService.findUser(friendAuthUser.getUserId())).willReturn(friendUser);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.of(friend));
+
+            // When & Then
+            assertThrows(FriendRequestAlreadySentException.class,
+                    () -> friendService.requestFriend(authUser.getUserId(), friendAuthUser.getUserId()));
+        }
+    }
+
+    @Nested
+    class 친구수락_테스트 {
+        @Test
+        void 친구수락_성공() {
+            // Given
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(userService.findUser(friendAuthUser.getUserId())).willReturn(friendUser);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.of(friend));
+            given(friendRepository.save(any(Friend.class)))
+                    .willAnswer(invocation -> invocation.getArgument(0));
+
+            // When
+            FriendResponseDto response = friendService.acceptFriend(authUser.getUserId(), friendAuthUser.getUserId());
+
+            // Then
+            assertThat(response).isNotNull();
+            assertThat(friend.getFriendStatus()).isEqualTo(FriendStatus.ACCEPTED);
+            verify(friendRepository).save(friend);
+        }
+
+        @Test
+        void 이미수락된_친구관계_예외발생() {
+            // Given
+            friend.acceptFriend();
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(userService.findUser(friendAuthUser.getUserId())).willReturn(friendUser);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.of(friend));
+
+            // When & Then
+            assertThrows(AlreadyFriendsException.class,
+                    () -> friendService.acceptFriend(authUser.getUserId(), friendAuthUser.getUserId()));
+        }
+    }
+
+    @Nested
+    class 친구목록조회_테스트 {
+        @Test
+        void 친구목록_조회_성공() {
+            // Given
+            AuthUser friendAuthUser2 = new AuthUser(3L, "user3@test.com", UserRole.ROLE_USER);
+            List<Friend> friends = Arrays.asList(
+                    friend,
+                    new Friend(user, User.fromAuthUser(friendAuthUser2))
+            );
+            Page<Friend> friendPage = new PageImpl<>(friends);
+
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(friendRepository.findFriendsByUserWithStatusOrder(eq(authUser.getUserId()), any(PageRequest.class)))
+                    .willReturn(friendPage);
+
+            // When
+            Page<FriendPageResponseDto> response = friendService.getFriends(authUser.getUserId(), 1, 10);
+
+            // Then
+            assertThat(response).isNotNull();
+            assertThat(response.getContent()).hasSize(2);
+        }
+    }
+
+    @Nested
+    class 친구삭제_테스트 {
+        @Test
+        void 친구삭제_성공() {
+            // Given
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.of(friend));
+
+            // When
+            friendService.deleteFriend(authUser.getUserId(), friendAuthUser.getUserId());
+
+            // Then
+            verify(friendRepository).delete(friend);
+        }
+
+        @Test
+        void 존재하지않는_친구관계_삭제시_예외발생() {
+            // Given
+            given(userService.findUser(authUser.getUserId())).willReturn(user);
+            given(friendRepository.findFriendRelationshipBetweenUsers(authUser.getUserId(), friendAuthUser.getUserId()))
+                    .willReturn(Optional.empty());
+
+            // When & Then
+            assertThrows(NotFoundFriendException.class,
+                    () -> friendService.deleteFriend(authUser.getUserId(), friendAuthUser.getUserId()));
+        }
+    }
+
+    @Nested
+    class 친구찾기_테스트 {
+        @Test
+        void 수락된_친구목록_조회_성공() {
+            // Given
+            AuthUser friendAuthUser2 = new AuthUser(3L, "user3@test.com", UserRole.ROLE_USER);
+            User friendUser2 = User.fromAuthUser(friendAuthUser2);
+            List<User> friends = Arrays.asList(friendUser, friendUser2);
+
+            given(friendRepository.findFriendsByUser(authUser.getUserId(), FriendStatus.ACCEPTED))
+                    .willReturn(friends);
+
+            // When
+            List<User> result = friendService.findFriends(authUser.getUserId());
+
+            // Then
+            assertThat(result).isNotNull();
+            assertThat(result).hasSize(2);
+            verify(friendRepository).findFriendsByUser(authUser.getUserId(), FriendStatus.ACCEPTED);
+        }
+    }
+}
