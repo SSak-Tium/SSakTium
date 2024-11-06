2a50eee3313cf5333377967644d1fbf83f837f69
Daegyu Han <bigstar77han@gmail.com>
2024-11-06 20:35:26 +0900

    cicd test - redirectUri html 전달

--- src/main/java/com/sparta/ssaktium/domain/users/customOauth/controller/CustomOauthController.java
+++ src/main/java/com/sparta/ssaktium/domain/users/customOauth/controller/CustomOauthController.java
@@ -6,7 +6,9 @@ import com.sparta.ssaktium.domain.users.customOauth.service.CustomOauthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,10 +17,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class CustomOauthController {
 
+    @Value("${app.domainUri}")
+    private String domainUri;
+
+
     private final CustomOauthService customOauthService;
 
     @GetMapping("/ssaktium/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("domainUri", domainUri);
         return "home";
     }
 
