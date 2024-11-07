cfbaa562e4d27ee3732fbc31456adfa10de3c37e
Xabier <94465466+XabierJEONG@users.noreply.github.com>
2024-11-07 21:34:34 +0900

    (FEAT) 알림기능 Producer 추가 및 Config 수정

--- src/main/java/com/sparta/ssaktium/domain/notifications/dto/NotificationMessage.java
+++ src/main/java/com/sparta/ssaktium/domain/notifications/dto/NotificationMessage.java
@@ -1,16 +1,14 @@
-package com.sparta.ssaktium.domain.notification.notificationMessage;
+package com.sparta.ssaktium.domain.notifications.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationMessage {
-
     private Long userId;
-    private String eventType;
+    private EventType eventType;
     private String message;
-
 }
